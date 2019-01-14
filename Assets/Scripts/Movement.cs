f94b6f26d182dacba45106c53bb1ec55e4068883
Oana Furtuna <oana.furtuna98@gmail.com>
2019-01-14 09:30:46 +0000

    First commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Vector3 direction;
+    Vector3 target;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        chooseDirection();       
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("c")) { chooseDirection(); }
+        moveRandomly();        
+    }
+
+
+    void chooseDirection()
+    {
+        direction = new Vector3(0, Random.Range(0, 360), 0);
+        transform.Rotate(direction);
+    }
+
+    void moveRandomly()
+    {
+        target = new Vector3(0, 0, direction.y);
+        transform.Translate(target*0.0006f);
+        animator.SetFloat("Idle", 0.5f);
+    }
+
+}
