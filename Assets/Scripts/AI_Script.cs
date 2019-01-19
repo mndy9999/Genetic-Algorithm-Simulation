0069706b3c66e029f4d65224a2e810f294913460
Oana Furtuna <oana.furtuna98@gmail.com>
2019-01-19 15:07:33 +0000

    Added eating behaviour to FSM

---
+++ Assets/Scripts/AI_Script.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Script : MonoBehaviour {
+
+    Animator anim;
+    public GameObject player;
+    float foodLevel = 0; 
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player) 
+        anim.SetFloat("distance", Vector3.Distance(transform.position, player.transform.position));
+        Debug.Log(foodLevel);
+    }
+
+    void eat()
+    {
+        foodLevel++;
+        anim.SetFloat("foodLevel", foodLevel);
+    }
+
+    public void startEating()
+    {
+        InvokeRepeating("eat", 0.5f, 0.5f);
+    }
+    public void stopEating()
+    {
+        CancelInvoke("eat");
+    }
+}
