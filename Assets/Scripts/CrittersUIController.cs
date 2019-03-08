a57ba7ea09f5aa9599a3e9532ebf413baa870fb0
Oana Furtuna <oana.furtuna98@gmail.com>
2019-03-08 11:03:38 +0000

    started adding UI for critters

---
+++ Assets/Scripts/CrittersUIController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrittersUIController : MonoBehaviour
+{
+
+    public Canvas canvas;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.GetMask("Critters")))
+            {
+                canvas.gameObject.SetActive(true);
+            }
+            else { canvas.gameObject.SetActive(false); }
+        }
+    }
+}
